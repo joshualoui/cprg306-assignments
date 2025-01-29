83be798b1dc005b130614a9595a158c9e8e76d57
joshualoui <joshua.louie@edu.sait.ca>
2025-01-29 14:24:34 -0700

    Functionally done
    TODO: gray out button, make it look nicer

--- app/week-4/page.js
+++ app/week-4/page.js
@@ -1,32 +1,33 @@
 "use client";
 
-import useState from "react";
+import {useState} from "react";
 
 export default function Page() {
     const [quantity, setQuantity] = useState(1);
     
     const increment = () => {
         setQuantity(quantity + 1);
-        if (quantity > 20) {
-            alert("You have reached the maximum value of the counter (20");
+        if (quantity >= 19) {
+            alert("You have reached the maximum value of the counter (20)");
         }
     }
 
     const decrement = () => {
         setQuantity(quantity - 1);
+        if (quantity <= 2) {
+            alert("You have reached the minimum value of the counter (1)");
+        }
     }
-
 
     return (
       <main>
-        <h1>week 4</h1>
+        <h1>Week 4</h1>
         <div>
             <p>Quantity: {quantity}</p>
-            <p>{count}</p>
             <p>The increment and decrement counter</p>
-            <button onClick={increment} className="p-2 m-2 bg-blue-500 hover:bg-blue-300 active:bg-yellow-400 w-38 rounded">Increment</button>
+            <button onClick={increment} className="p-2 m-2 bg-blue-500 hover:bg-blue-300 active:bg-yellow-400 w-38 rounded" disabled={quantity>=20}>Add 1</button>
 
-            <button onClick={decrement} className="p-2 m-2 bg-red-500 hover:bg-red-300 active:bg-yellow-400 w-38 rounded">Decrement</button>
+            <button onClick={decrement} className="p-2 m-2 text-yellow-300 bg-red-500 hover:bg-red-300 active:bg-yellow-400 w-38 rounded" disabled={quantity<=1}>Minus 1</button>
         </div>
       </main>
     );
