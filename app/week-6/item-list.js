481080802be5bad933f5cae3423777cf99f44ece
joshualoui <joshua.louie@edu.sait.ca>
2025-03-01 22:39:40 -0700

    Fixed
    the sortby is in the return inside of my handleClick statements

--- app/week-6/item-list.js
+++ app/week-6/item-list.js
@@ -8,24 +8,20 @@ export default function List() {
   const [toggleName, setToggleName] = useState(false);
   const [toggleCategory, setToggleCategory] = useState(false);
 
-  // itemData.sort((a, b) => a.name.localeCompare(b.name));
-  // itemData.sort((a, b) => a.category.localeCompare(b.category));
+  //created new array to not change orignial json.
+  const sortedItemName = [...itemData].sort((a, b) =>
+    a.name.localeCompare(b.name)
+  );
 
-  const handleClickName = (click) => {
-    itemData.sort((a, b) => a.name.localeCompare(b.name));
-    setSortBy(click);
-  };
-
-  const handleClickCategory = (click) => {
-    itemData.sort((a, b) => a.category.localeCompare(b.category));
-    setSortBy(click);
-  };
+  const sortedItemCategory = [...itemData].sort((a, b) =>
+    a.category.localeCompare(b.category)
+  );
 
   return (
     <main>
       <button
         onClick={() => {
-          handleClickName("name");
+          setSortBy("name");
           setToggleName(true);
           setToggleCategory(false);
         }}
@@ -35,19 +31,21 @@ export default function List() {
       >
         Name
       </button>
+
       <button
         onClick={() => {
-          handleClickCategory("category");
+          setSortBy("category");
           setToggleCategory(true);
           setToggleName(false);
         }}
         className={`p-2 m-2 text-teal-600 ${
-          toggleCategory ? " bg-blue-700 " : "bg-gray-800"
+          toggleCategory ? "bg-blue-700" : "bg-slate-800"
         } hover:bg-gray-600 w-38 rounded`}
       >
         Category
       </button>
-      {itemData.map((item) => (
+
+      {(sortBy === "name" ? sortedItemName : sortedItemCategory).map((item) => (
         <Item
           key={item.id}
           name={item.name}
