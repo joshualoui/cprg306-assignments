4a830a8ea83739e60ee672eadb3f1fc8a9b6275a
joshualoui <joshua.louie@edu.sait.ca>
2025-03-09 22:40:35 -0600

    Finished Step 4 modify  new-item.js
    item.js has the properties so think this should be good: The item object should have the following properties: id, name, quantity, and category. id should be a random string.

--- app/week-7/page.js
+++ app/week-7/page.js
@@ -1,15 +1,25 @@
 "use client";
 import List from "./item-list.js";
 import Link from "next/link";
+import Counter from "./new-item.js";
+import itemsData from "./items.json";
+import { useState } from "react";
 
 export default function Page() {
+  const [items, setItems] = useState(itemsData);
+
+  const handleAddItem = (item) => {
+    setItems([...items, item]);
+  };
+
   return (
     <main>
       <h1 className="font-bold text-teal-600 text-4xl">Shopping List</h1>
       <p className="hover:text-sky-400 underline">
         <Link href="/">Home</Link>{" "}
       </p>
-      <List />
+      <Counter onAddItem={handleAddItem} />
+      <List newItem={items} />
     </main>
   );
 }
