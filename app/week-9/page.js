c91dad1151042c8f7874d4b57813b44d4711b6ec
joshualoui <joshua.louie@edu.sait.ca>
2025-03-22 19:24:21 -0600

    Finished
    Maybe add styling? its kind of ugly

--- app/week-9/page.js
+++ app/week-9/page.js
@@ -1,20 +1,58 @@
 "use client";
 import { useUserAuth } from "./_utils/auth-context";
+import Link from "next/link";
 
 export default function Page() {
   const { user, gitHubSignIn, firebaseSignOut } = useUserAuth();
 
   return (
     <main>
-      <p>Not logged into account page</p>
-
       <p>
         {user ? (
-          <button onClick={firebaseSignOut}>Log Out</button>
+          <div>
+            {" "}
+            {user.displayName} do you want to:{" "}
+            <button
+              className="hover:text-sky-400 underline"
+              onClick={firebaseSignOut}
+            >
+              Log Out?
+            </button>
+          </div>
         ) : (
-          <button onClick={gitHubSignIn}>Sign In</button>
+          <div>
+            <button
+              className="hover:text-sky-400 underline"
+              onClick={gitHubSignIn}
+            >
+              Sign In
+            </button>
+          </div>
         )}
       </p>
+      {user ? (
+        <div>
+          Logged in as {user.displayName}. Email: {user.email}. Last time you
+          signed in was: {user.metadata.lastSignInTime}. Your pfp is
+          <img src={user.photoURL} /> Come view our{" "}
+          <Link
+            className="hover:text-sky-400 underline"
+            href="week-9/shopping-list"
+          >
+            Secure Shopping List Page!
+          </Link>
+        </div>
+      ) : (
+        <div>
+          Try and get into this page without login.{" "}
+          <Link
+            className="hover:text-sky-400 underline"
+            href="week-9/shopping-list"
+          >
+            Secure Shopping List Page
+          </Link>
+        </div>
+      )}
     </main>
   );
 }
--- app/week-9/shopping-list/page.js
+++ app/week-9/shopping-list/page.js
@@ -5,10 +5,12 @@ import Link from "next/link";
 import Counter from "../../week-8/new-item.js";
 import itemsData from "./items.json";
 import { useState } from "react";
+import { useUserAuth } from "../_utils/auth-context";
 
 export default function Page() {
   const [items, setItems] = useState(itemsData);
   const [selectedItemName, setSelectedItemName] = useState(null);
+  const { user } = useUserAuth();
 
   const handleItemSelect = (item) => {
     const cleanName = item
@@ -26,21 +28,39 @@ export default function Page() {
     setItems([...items, item]);
   };
 
-  return (
-    <main>
-      <h1 className="font-bold text-teal-600 text-4xl">Shopping List</h1>
-      <p className="hover:text-sky-400 underline">
-        <Link href="/">Home</Link>{" "}
-      </p>
-      <div className="flex">
-        <div className="flex-2">
-          <Counter onAddItem={handleAddItem} />
-          <List onItemSelect={handleItemSelect} items={items} />
+  if (!user) {
+    return (
+      <main>
+        <div>You're not allowed to view the shopping list!!</div>
+        <div>
+          {" "}
+          Time to go{" "}
+          <Link className="hover:text-sky-400 underline" href="/week-9">
+            BACK
+          </Link>
         </div>
-        <div className="flex pl-20">
-          <MealIdeas ingredient={selectedItemName} />
+      </main>
+    );
+  } else {
+    return (
+      <main>
+        <h1 className="font-bold text-teal-600 text-4xl">Shopping List</h1>
+        <p className="hover:text-sky-400 underline">
+          <Link href="/">Home</Link>{" "}
+        </p>
+        <p className="hover:text-sky-400 underline">
+          <Link href="/week-9">Back to week 9</Link>{" "}
+        </p>
+        <div className="flex">
+          <div className="flex-2">
+            <Counter onAddItem={handleAddItem} />
+            <List onItemSelect={handleItemSelect} items={items} />
+          </div>
+          <div className="flex pl-20">
+            <MealIdeas ingredient={selectedItemName} />
+          </div>
         </div>
-      </div>
-    </main>
-  );
+      </main>
+    );
+  }
 }
