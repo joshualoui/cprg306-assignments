1d80ed23a7259e6338e2f3c1df022ee504c185e0
joshualoui <joshua.louie@edu.sait.ca>
2025-04-17 18:53:40 -0600

    Working with wrapper query
    About to implement pagination

--- app/pokemonTCG/components/cards.js
+++ app/pokemonTCG/components/cards.js
@@ -1,13 +1,27 @@
+// const fetchCards = async (name, setCards) => {
+//   try {
+//     const apiCall = await fetch(
+//       `https://api.pokemontcg.io/v2/cards?q=name:${name}`
+//     );
+//     const pokemon = await apiCall.json();
+//     setCards(pokemon.data);
+//   } catch (error) {
+//     console.error("Error fetching cards:", error);
+//     setCards([]);
+//   }
+// };
+// export default fetchCards;
+
+import pokemon from "../lib/pokemonClient";
+
 const fetchCards = async (name, setCards) => {
   try {
-    const apiCall = await fetch(
-      `https://api.pokemontcg.io/v2/cards?q=name:${name}`
-    );
-    const pokemon = await apiCall.json();
-    setCards(pokemon.data);
+    const result = await pokemon.card.where({ q: `name:${name}` });
+    setCards(result.data);
   } catch (error) {
     console.error("Error fetching cards:", error);
     setCards([]);
   }
 };
+
 export default fetchCards;
